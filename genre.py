ec52141b2734d881f862b5245e7114e6942cb278
nyla <lany.downs@gmail.com>
2024-12-17 12:54:28 -0500

    nyla-lab4

---
+++ genre.py
@@ -0,0 +1,12 @@
+#Genres Class
+class Genre:
+    def __init__(self, title: str, artist: str):
+        self.title = title 
+        self.artist = artist 
+
+    def
+    def play(self):
+        print(self.title + ' by ' + self.artist + ' plays')
+
+
+    
--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-
+from genre import Genre
 
 if __name__ == '__main__':
     print('new dawn, new day')
+    my_genre = Genre('HUMBLE', 'Kendrick Lamar')
+    my_genre.play()
