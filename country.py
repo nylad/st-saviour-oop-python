6302f19984154755058074aa220a95d57cf10aee
nyla <lany.downs@gmail.com>
2025-01-10 18:39:01 -0500

    nyla-lab4

--- country.py
+++ country.py
@@ -1,5 +1,16 @@
 from song import Song
 
 class Country(Song): 
-    def __init__(self, title: str artist : str) :
-        super().__init__(title, artist)
+    def __init__(self, title: str artist : str, slower_speed: str):
+    super(). __init__("title", "artist")
+    self.slower_speed = slower_speed
+    
+    def add_slower_speed (self, *arg):
+        for arg in args:
+            self._slower_speed.append(arg)
+
+        
+    
+
+
+    
